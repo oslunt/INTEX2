459ae29799acb1a840ff4bccb20cff6845e75137
Benjamin Kloepfer <90466204+benjark@users.noreply.github.com>
2022-04-04 10:42:39 -0600

    1.0

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,8 @@ namespace INTEX2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,8 @@ namespace INTEX2
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
             });
         }
     }
