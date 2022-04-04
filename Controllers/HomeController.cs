4926b672cb2bb2989cf739178dfef7edf8e2ccf8
oslunt <77417770+oslunt@users.noreply.github.com>
2022-04-04 08:51:10 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using INTEX2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEX2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
