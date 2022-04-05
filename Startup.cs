e51f3cfd1889954967a0dedb45723b9c529b083b
Benjamin Kloepfer <90466204+benjark@users.noreply.github.com>
2022-04-05 15:56:19 -0600

    Updated MFA Almost there

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,14 @@ namespace INTEX2
         public void ConfigureServices(IServiceCollection services)
 
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential 
+                // cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                // requires using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
             services.ConfigureApplicationCookie(options =>
             {
                 // Cookie settings
